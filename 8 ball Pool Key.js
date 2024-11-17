6573b053eba37fdc32696c9708de935dd493ec6a
Mahdi777-Hex <mahdi.1111lahbali@gmail.com>
2024-11-17 14:50:27 +0100

    Add files via upload

---
+++ 8 ball Pool Key.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@SH4ko1" 
+ while (sWord != "SH4KO_7WAY") {// Pass
+ sWord = prompt("⇰ 𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝐁𝐘 ⪼ 𝐒𝐡𝟒𝐊𝐨  ");
+
+ } 
